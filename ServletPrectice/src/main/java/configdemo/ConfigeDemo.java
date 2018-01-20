d9d7ebb4e622b2bf7d5b372a0ccfaf141e268e80
Maitri <maitrivelani999@gmail.com>
2018-01-20 17:25:32 +0530

    All classes are done with annotation

--- ServletPrectice/src/main/java/configdemo/ConfigeDemo.java
+++ ServletPrectice/src/main/java/configdemo/ConfigeDemo.java
@@ -2,12 +2,15 @@ package configdemo;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+//@WebServlet(value = "/config")
 public class ConfigeDemo extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
